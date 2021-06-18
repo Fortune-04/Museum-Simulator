084c1775b92469c076425f3442fa6792c24bae9c
Fortune-04 <spiritlance48@gmail.com>
2021-06-18 19:17:47 +0800

    first commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+//import from java fx
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
@@ -26,7 +27,7 @@ public class Controller {
     public Text totalVisMus;
 
 
-
+    // method to start the program
     public void startMuseum() {
 
         ExecutorService service = Executors.newFixedThreadPool(6);
@@ -52,54 +53,67 @@ public class Controller {
         curTime.setText(time);
     }
 
+    //print the museum not open
     public void notOpen(String not_open){
         ticketCounter.appendText(not_open+"\n");
     }
 
+    //append text to print the ticket sold
     public void ticketSold(String ticket_sold){
         ticketCounter.appendText(ticket_sold);
     }
 
+    //append text to print the museum close or open
     public void openClose(String open_close){
         ticketCounter.appendText(open_close+"\n");
     }
 
+    //print the ticket id for west exit gate
     public void exitWestGate(String exit_west_gate){
         westGate.appendText(exit_west_gate+"\n");
     }
 
+    //print the ticket id for east exit gate
     public void exitEastGate(String exit_east_gate){
         eastGate.appendText(exit_east_gate+"\n");
     }
 
+    //print the ticket id for north entrance gate
     public void entranceNorthGate(String entrance_north_gate){
         northGate.appendText(entrance_north_gate+"\n");
     }
 
+    //print the ticket id for south entrance gate
     public void entranceSouthGate(String entrance_south_gate){
         southGate.appendText(entrance_south_gate+"\n");
     }
 
+    //append text to print the current time
     public void time(String time){
         curTime.setText(time);
     }
 
+    //append text to print the number of current visitor
     public void curVisitor(String cur_visitor){
         curVis.setText(cur_visitor);
     }
 
+    //append text to print the total ticket sold
     public void totalTicket(String total_ticket){
         tcRe.setText(total_ticket);
     }
 
+    //append text to print the waiting list of visitor
     public void waitList(String wait_list){
         waitList.setText(wait_list);
     }
 
+    //append text to print the total visitor visit the museum
     public void totalVisitorMuseum(String total_vis_museum){
         totalVisMus.setText(total_vis_museum);
     }
 
+    //method for close the program
     public void exitMuseum(){
         System.exit(1);
     }
